b67c42ee95c86549a00b3265730b3fab149ee8f0
Kat-tech15 <kelvinkatwai@gmail.com>
2025-11-07 11:50:37 +0300

    added livestock app

--- livestock/models.py
+++ livestock/models.py
@@ -1,3 +1,20 @@
 from django.db import models
+from accounts.models import CustomUser
 
-# Create your models here.
+class Livestock(models.Model):
+    CATEGORY_CHOICES = [
+        ('cattle', 'Cattle'),
+        ('goat', 'Goat'),
+        ('donkey', 'Donkey'),
+        ('pig', 'Pig'),
+        ('poultry', 'Poultry')
+    ]
+    owner = models.ForeignKey(CustomUser, on_delete=models.CASACADE)
+    name  = models.CharField(max_length=50)
+    category = models.CharField(max_length=100, choices=CATEGORY_CHOICES)
+    image  = models.ImageField(upload_to='livestock_images/', null=True, blank=True)
+    age  = models.PositiveIntegerField(default=0)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"{self.name} - {self.category}"
--- livestock/views.py
+++ livestock/views.py
@@ -1,3 +1,33 @@
-from django.shortcuts import render
+from rest_framework import generics, permissions, mixins
+from accounts.permissions import IsOwnerOrReadOnly
+from .serializers import LivestockSerializer
+from .models import Livestock
 
-# Create your views here.
+class LivestockList(mixins.ListModelMixin, mixins.CreateModelMixin, generics.GenericAPIView):
+    queryset = Livestock.objects.all()
+    serializer_class = LivestockSerializer
+
+    def get(self, request, *args, **kwargs):
+        return self.list(request, *args, **kwargs)
+
+    def post(self, request, *args, **kwargs):
+        return self.create(request, *args, **kwargs)
+
+
+class LivestockDetail(mixins.RetriveModelMixin,
+                      mixins.UpdateModelMixin, 
+                      mixins.DestroyModelMixin, 
+                      generics.GenericAPIView
+                    ):
+
+    queryset = Livestock.objects.all()
+    serializer_class = LivestockSerializer
+
+    def get(self, request, *args, **kwargs):
+        return self.retrieve(request, *args, **kwargs)
+    
+    def put(self, request, *args, **kwargs):
+        return self.update(request, *args, **kwargs)
+
+    def delete(self, request, *args, **kwargs):
+        return self.destroy(request, *args, **kwargs)
