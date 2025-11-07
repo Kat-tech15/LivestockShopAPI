a2ded49e26d07ec1f2c33166098f7caf7be2f4d4
Kat-tech15 <kelvinkatwai@gmail.com>
2025-11-07 11:47:15 +0300

    added accounts app

--- accounts/views.py
+++ accounts/views.py
@@ -4,13 +4,13 @@ from rest_framework.response import Response
 from .serializers import RegisterSerializer
 from rest_framework.authtoken.models import Token
 from django.contrib.auth import authenticate
-from .models import CustomUser
+
 
 class RegisterView(APIView):
     def post(self,request):
         serializer = RegisterSerializer(data=request.data)
         if serializer.is_valid():
-            serializer.save(user=user)
+            user = serializer.save()
             token, _ = Token.objects.get_or_create(user=user)
             return Response({'message':'User created successfully!'})
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
@@ -18,5 +18,25 @@ class RegisterView(APIView):
 class LoginView(APIView):
     def post(request):
         username = request.data.get('username')
-        password = request.ddata.get()
-        
+        password = request.ddata.get('password')
+
+        user = authenticate(username=username, password=password)
+
+        if user:
+            token, created = Token.objects.get_or_create(user=user)
+            return Response({
+                'token': token.key,
+                'username': user.username,
+                'email': user.email
+            })
+        return Response({'message': 'Invalid credentials!'}, status=status.HTTP_401_UNAUTHORIZED)
+         
+class LogoutView(APIView):
+    parser_classes = [permissions.IsAuthenticated]
+
+    def get(self, request):
+        try:
+            request.user.auth_token.delete()
+        except:
+            pass
+        return Response({'message': 'User logged out successfully!'}, status=status.HTTP_200_OK)
