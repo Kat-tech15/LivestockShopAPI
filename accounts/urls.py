1b7469df74209e4175dc7b41a69db865d2eded89
Katwai <kelvinkatwai@gmail.com>
2025-12-09 10:42:33 +0300

    added jwt urls to path

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,8 +1,16 @@
 from django.urls import path
 from . import views
+from rest_framework_simplejwt.views import (
+    TokenObtainPairView,    
+    TokenRefreshView,
+    TokenVerifyView,
+)
 
 urlpatterns = [
     path('register/', views.RegisterView.as_view()),
     path('login/', views.LoginView.as_view()),
     path('logout/', views.LogoutView.as_view()),
+    path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+    path('api/token/verify/', TokenVerifyView.as_view(), name='token_verify'),
 ]
